d6c8213fa379727e91a36c8a986b94c5e8b1f39b
Kushaal Tummala <tummala.kushaal01@gmail.com>
2021-02-20 22:29:58 +0530

    minor fixes

--- open_class.py
+++ open_class.py
@@ -11,9 +11,12 @@ from send_alert import send_alert
 join_now_path = "//span[@class='NPEfkd RveJvd snByac']"
 end_class_path = "//span[@class='DPvwYc JnDFsc grFr5 FbBiwc']"
 record_join_now_path = "//div[@class='']"
-def open_class(link, runtime, class_name, record_class=False, debug_port = 6969, exec_path = "D:\Github\chromedriver_win32\chromedriver.exe", user_data_dir = "D:\Github\Auto_Meet\sel_profile", exit_browser = False):
+def open_class(link, runtime, class_name, record_class=False, debug_port = 6942, exec_path = "D:\Github\chromedriver_win32\chromedriver.exe", user_data_dir = "D:\Github\Auto_Meet\sel_profile", exit_browser = False):
+    if "https://" not in link:
+        link = "https://" + link
+    
     debug_address = "127.0.0.1:" + str(debug_port)
-    cmd = "chrome --remote-debugging-port=" + str(debug_port) + "user_data_dir=" + user_data_dir
+    cmd = "chrome --remote-debugging-port=" + str(debug_port) + " --user-data-dir=" + user_data_dir
     chrome_options = Options()
     chrome_options.add_experimental_option("debuggerAddress", debug_address)
     chrome_options.add_argument("--mute-audio")
@@ -40,19 +43,12 @@ def open_class(link, runtime, class_name, record_class=False, debug_port = 6969,
     #Mic and cam off check
     while True:
         if "Turn on microphone" in src and "Turn on camera" in src:
-            mic_off = True
-            cam_off = True
             break
-        if "Turn off microphone" in src:
-            mic_off = False
+        else:
             driver.refresh()
-            sleep(7)
+            sleep(3)
             src = driver.page_source
-        if "Turn off camera" in src:
-            cam_off = False
-            driver.refresh()
-            sleep(7)
-            src = driver.page_source
+
     #join_class
     try:
         j = driver.find_element_by_xpath(join_now_path)
@@ -61,12 +57,29 @@ def open_class(link, runtime, class_name, record_class=False, debug_port = 6969,
     except:
         print("Error\nCouldnt find join button")
         send_alert(joined_class=False, class_name=class_name)
+    
+    #Check for join in case of recording
+    '''
+    src = driver.page_source
+    if "This meeting is being recorded" in src:
+        try:
+            r = driver.find_element_by_xpath(record_join_now_path)
+            r.click()
+        except:
+            send_alert(custom_msg = "Joined class but couldnt get past record screen")
+    '''
     driver.quit()
 
     sleep(runtime) #Sleep for the duration of the class
     
     #Connect again to exit class
-    driver = webdriver.Chrome(options=chrome_options, executable_path= exec_path)
+    try:
+        print("Connecting to driver")
+        driver = webdriver.Chrome(options=chrome_options, executable_path= exec_path)
+        print("Connected")
+    except:
+        print("Couldnt connect to driver")
+        send_alert(exit_class=False)
     try:
         l = driver.find_element_by_xpath(end_class_path)
         l.click()
